366cc05995f37412cfc961fec782156ba1d88687
Steve T <steve.torrefranca@investree.ph>
2023-11-15 10:19:56 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={antic.className}>
       <body suppressHydrationWarning={true}>
-        <GoogleAnalytics />
+        <GoogleAnalytics /> 
         {children}
       </body>
     </html>
